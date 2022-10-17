babfe6cc9445944239844f66e9ec63279236b6d1
alyrow <58048578+alyrow@users.noreply.github.com>
2022-10-17 12:54:02 +0200

    build frontend and link it to the backend

--- lpmng-core/run.sh
+++ lpmng-core/run.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+if [ ! "$(ls -A src/public)" ]; then
+    cd ../lpmng-front
+    yarn run nuxt generate
+    cp -R dist/ -T ../lpmng-core/src/public
+    cd -
+fi
 export DATABASE_URL=postgres://corpau@localhost/lpmng
 sqlx migrate run
 cargo sqlx prepare
