72bb72940acc9845d7555b6608f662f42da705fb
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2022-10-19 19:18:10 +0200

    remake sessions migration

---
+++ lpmng-core/migrations/20221019171534_sessions.sql
@@ -0,0 +1,9 @@
+create table if not exists sessions
+(
+    id          uuid    default gen_random_uuid() not null primary key,
+    ip4         text                              not null,
+    mac         text                              not null,
+    user_id     uuid,
+    internet    boolean default false             not null,
+    date_tome   timestamp                         not null
+);
