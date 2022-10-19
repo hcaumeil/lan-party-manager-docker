1d15e390ca7620a43c80c13a903d6193780ae4c3
alyrow <58048578+alyrow@users.noreply.github.com>
2022-10-19 10:19:40 +0200

    cleanify and add table editing

---
+++ lpmng-front/pages/admin/users.vue
@@ -0,0 +1,336 @@
+<template>
+  <v-card
+    class="rounded-lg admin-card"
+  >
+    <v-card-title>
+      Utilisateurs
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Recherche"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      :search="search"
+    >
+      <template v-slot:top>
+        <v-toolbar
+          flat
+        >
+          <v-toolbar-title>My CRUD</v-toolbar-title>
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          ></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog
+            v-model="dialog"
+            max-width="500px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                dark
+                class="mb-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                New Item
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-text-field
+                    v-model="editedItem.username"
+                    :rules="[rules.required]"
+                    name="username"
+                    placeholder="Nom d'utilisateur"
+                    prepend-inner-icon="mdi-account"
+                    :disabled="loading"
+                    outlined
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="editedItem.firstname"
+                    :rules="[rules.required]"
+                    name="firstname"
+                    placeholder="Prénom"
+                    prepend-inner-icon="mdi-account"
+                    :disabled="loading"
+                    outlined
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="editedItem.lastname"
+                    :rules="[rules.required]"
+                    name="lastname"
+                    placeholder="Nom de famille"
+                    prepend-inner-icon="mdi-account"
+                    :disabled="loading"
+                    outlined
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="editedItem.email"
+                    :rules="[rules.required, rules.email]"
+                    name="email"
+                    placeholder="Addresse email"
+                    prepend-inner-icon="mdi-email"
+                    type="email"
+                    :disabled="loading"
+                    outlined
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="editedItem.phone"
+                    :rules="[rules.required, rules.phone]"
+                    name="phone"
+                    placeholder="Numéro de téléphone"
+                    prepend-inner-icon="mdi-phone"
+                    type="phone"
+                    :disabled="loading"
+                    outlined
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="editedItem.role"
+                    :rules="[rules.required, rules.roles]"
+                    name="role"
+                    placeholder="Role"
+                    prepend-inner-icon="mdi-account-question"
+                    :disabled="loading"
+                    outlined
+                  ></v-text-field>
+                  <v-switch
+                    v-model="editedItem.is_allowed"
+                    label="Accès à internet"
+                  ></v-switch>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="close"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="save"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:item.is_allowed="{ item }">
+        <v-simple-checkbox
+          v-model="item.is_allowed"
+          disabled
+        ></v-simple-checkbox>
+      </template>
+
+      <template v-slot:item.actions="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          small
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn
+          color="primary"
+          @click="initialize"
+        >
+          Reset
+        </v-btn>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'AdminUsers',
+  data () {
+    return {
+      search: '',
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        {
+          text: 'ID',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        {
+          text: 'Nom d\'utilisateur',
+          value: 'username'
+        },
+        {
+          text: 'Prénom',
+          value: 'firstname'
+        },
+        {
+          text: 'Nom de famille',
+          value: 'lastname'
+        },
+        {
+          text: 'Addresse email',
+          value: 'email'
+        },
+        {
+          text: 'Numéro de téléphone',
+          value: 'phone'
+        },
+        {
+          text: 'Role',
+          value: 'role'
+        },
+        {
+          text: 'Accès à internet',
+          value: 'is_allowed'
+        },
+        {
+          text: 'Actions',
+          value: 'actions',
+          sortable: false
+        },
+      ],
+      users: [],
+      editedIndex: -1,
+      editedItem: {
+        username: '',
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        role: '',
+        is_allowed: '',
+      },
+      defaultItem: {
+        username: '',
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        role: '',
+        is_allowed: '',
+      },
+      rules: {
+        required: value => !!value || 'Requis.',
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Addresse email invalide.'
+        },
+        phone: value => {
+          const pattern = /^\+33[67]\d{8}$|^0[67]\d{8}$/
+          return pattern.test(value) || 'Numéro de téléphone invalide.'
+        },
+        roles: value => value === 'user' || value === 'admin' || 'Mauvais roles.'
+      },
+      loading: false
+    }
+  },
+  middleware: 'auth',
+  created () {
+    this.$store.getters['api/users'].then(d => this.users = d)
+  },
+  methods: {
+    editItem (item) {
+      this.editedIndex = this.users.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem (item) {
+      this.editedIndex = this.users.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm () {
+      this.users.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete () {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.users[this.editedIndex], this.editedItem)
+      } else {
+        this.users.push(this.editedItem)
+      }
+      this.close()
+    },
+  },
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    },
+  },
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
+    },
+  },
+}
+</script>
+
+<style>
+.admin-card {
+  width: 100%;
+  max-width: 100%;
+}
+</style>
