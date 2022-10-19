6a4f558358743236339b53bfe945d647ee9ae355
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2022-10-19 19:17:29 +0200

    update table migration

--- lpmng-core/migrations/20221015183201_sessions.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists sessions
-(
-    id          uuid    default gen_random_uuid() not null primary key,
-    ip4         text                              not null,
-    mac         text                              not null,
-    user_id     uuid,
-    internet    boolean default false             not null
-);
--- lpmng-core/migrations/20221019164002_session_date.sql
+++
@@ -1,2 +0,0 @@
-alter table sessions
-add created timestamp not null default now();
