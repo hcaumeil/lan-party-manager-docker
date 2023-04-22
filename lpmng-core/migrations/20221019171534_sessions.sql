caf2f158e63379595dbc259edb3f0094aa4c8b24
alyrow <58048578+alyrow@users.noreply.github.com>
2023-04-22 19:08:16 +0200

    add more constraints on sessions table

--- lpmng-core/migrations/20221019171534_sessions.sql
+++ lpmng-core/migrations/20221019171534_sessions.sql
@@ -1,8 +1,8 @@
 create table if not exists sessions
 (
     id          uuid    default gen_random_uuid() not null primary key,
-    ip4         text                              not null,
-    user_id     uuid,
-    internet    boolean default false             not null,
+    ip4         text                              not null unique,
+    user_id     uuid references users,
+    internet    boolean default false             not null unique,
     date_time   timestamp                         not null default now()
 );
