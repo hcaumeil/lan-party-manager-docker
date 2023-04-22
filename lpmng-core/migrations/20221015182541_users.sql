7d1480964ce04071330241a1ea6604da037d5393
alyrow <58048578+alyrow@users.noreply.github.com>
2023-04-22 15:15:08 +0200

    add more constraints on users table

--- lpmng-core/migrations/20221015182541_users.sql
+++ lpmng-core/migrations/20221015182541_users.sql
@@ -1,12 +1,14 @@
+create extension if not exists pgcrypto;
+
 create table if not exists users
 (
     id          uuid    default gen_random_uuid() not null primary key,
-    username    text                              not null,
+    username    text                              not null unique,
     firstname   text                              not null,
     lastname    text                              not null,
-    email       text                              not null,
+    email       text                              not null unique,
     password    text                              not null,
-    phone       text                              not null,
+    phone       text                              not null unique,
     role        text                              not null,
     is_allowed  boolean default false             not null
 );
