b1d96e8c71df2ecfc9eb8a724ab8425a16b14d0d
alyrow <58048578+alyrow@users.noreply.github.com>
2023-04-25 08:07:18 +0200

    [lpmng-front] add api logout and refresh method to no-internet page

--- lpmng-front/pages/no-internet.vue
+++ lpmng-front/pages/no-internet.vue
@@ -7,30 +7,38 @@
     style="backdrop-filter: blur(30px); background-color: #1e1e1eaa"
   >
     <v-card-actions>
-      <NuxtLink to="/login">
+      <v-btn
+        icon
+        @click="logout"
+      >
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+      <v-spacer />
+      <NuxtLink to="/check">
         <v-btn
           icon
         >
-          <v-icon>mdi-arrow-left</v-icon>
+          <v-icon>mdi-refresh</v-icon>
         </v-btn>
       </NuxtLink>
-
     </v-card-actions>
     <v-card-title>Pas d'internet :/</v-card-title>
     <v-container>
-      <p>Vous n'avez pas été autorisé à acceder à internet. Merci d'aller dans la salle {{ this.room }} pour demander
-        l'accès.</p>
+      <p>
+        Vous n'avez pas été autorisé à accéder à internet. Merci d'aller dans la salle {{ room }} pour demander
+        l'accès.
+      </p>
     </v-container>
     <v-card-actions
       v-if="$store.getters['api/is_admin']"
     >
-      <v-spacer></v-spacer>
+      <v-spacer />
       <NuxtLink to="/admin">
         <v-btn
           depressed
           color="primary"
         >
-          Acceder à la console admin
+          Accéder à la console admin
         </v-btn>
       </NuxtLink>
     </v-card-actions>
@@ -41,11 +49,28 @@
 export default {
   name: 'NoInternet',
   layout: 'kiosk',
+  middleware: ['auth', 'bypass'],
   data () {
     return {
-      room: 'E...',
+      room: 'E...'
     }
   },
-  middleware: 'auth'
+  methods: {
+    async logout () {
+      const me = await this.$store.getters['api/user']()
+      const session = await this.$store.getters['api/session']()
+      if (session != null) {
+        await this.$store.getters['api/post_session']({
+          id: session.id,
+          ip4: '',
+          user_id: me.id,
+          internet: false,
+          date_time: (new Date()).toISOString().replace('Z', '')
+        })
+      }
+      localStorage.clear()
+      location.reload()
+    }
+  }
 }
 </script>
