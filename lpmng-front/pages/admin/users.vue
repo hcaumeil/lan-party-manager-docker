ebee3b209f679baa9443e466dd8c3d2fb9ff1970
alyrow <58048578+alyrow@users.noreply.github.com>
2023-04-25 08:05:19 +0200

    [lpmng-front] add api logic to users page

--- lpmng-front/pages/admin/users.vue
+++ lpmng-front/pages/admin/users.vue
@@ -4,21 +4,21 @@
   >
     <v-card-title>
       Utilisateurs
-      <v-spacer></v-spacer>
+      <v-spacer />
       <v-text-field
         v-model="search"
         append-icon="mdi-magnify"
         label="Recherche"
         single-line
         hide-details
-      ></v-text-field>
+      />
     </v-card-title>
     <v-data-table
       :headers="headers"
       :items="users"
       :search="search"
     >
-      <template v-slot:top>
+      <template #top>
         <v-toolbar
           flat
         >
@@ -41,7 +41,7 @@
                     prepend-inner-icon="mdi-account"
                     :disabled="loading"
                     outlined
-                  ></v-text-field>
+                  />
                   <v-text-field
                     v-model="editedItem.firstname"
                     :rules="[rules.required]"
@@ -50,7 +50,7 @@
                     prepend-inner-icon="mdi-account"
                     :disabled="loading"
                     outlined
-                  ></v-text-field>
+                  />
                   <v-text-field
                     v-model="editedItem.lastname"
                     :rules="[rules.required]"
@@ -59,7 +59,7 @@
                     prepend-inner-icon="mdi-account"
                     :disabled="loading"
                     outlined
-                  ></v-text-field>
+                  />
                   <v-text-field
                     v-model="editedItem.email"
                     :rules="[rules.required, rules.email]"
@@ -69,7 +69,7 @@
                     type="email"
                     :disabled="loading"
                     outlined
-                  ></v-text-field>
+                  />
                   <v-text-field
                     v-model="editedItem.phone"
                     :rules="[rules.required, rules.phone]"
@@ -79,7 +79,7 @@
                     type="phone"
                     :disabled="loading"
                     outlined
-                  ></v-text-field>
+                  />
                   <v-text-field
                     v-model="editedItem.role"
                     :rules="[rules.required, rules.roles]"
@@ -88,16 +88,16 @@
                     prepend-inner-icon="mdi-account-question"
                     :disabled="loading"
                     outlined
-                  ></v-text-field>
+                  />
                   <v-switch
                     v-model="editedItem.is_allowed"
                     label="Accès à internet"
-                  ></v-switch>
+                  />
                 </v-container>
               </v-card-text>
 
               <v-card-actions>
-                <v-spacer></v-spacer>
+                <v-spacer />
                 <v-btn
                   color="blue darken-1"
                   text
@@ -117,26 +117,32 @@
           </v-dialog>
           <v-dialog v-model="dialogDelete" max-width="500px">
             <v-card>
-              <v-card-title class="text-h5">Voulez-vous vraiment supprimez cet utilisateur?</v-card-title>
+              <v-card-title class="text-h5">
+                Voulez-vous vraiment supprimez cet utilisateur?
+              </v-card-title>
               <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Annuler</v-btn>
-                <v-btn color="red darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
+                <v-spacer />
+                <v-btn color="blue darken-1" text @click="closeDelete">
+                  Annuler
+                </v-btn>
+                <v-btn color="red darken-1" text @click="deleteItemConfirm">
+                  OK
+                </v-btn>
+                <v-spacer />
               </v-card-actions>
             </v-card>
           </v-dialog>
         </v-toolbar>
       </template>
 
-      <template v-slot:item.is_allowed="{ item }">
+      <template #item.is_allowed="{ item }">
         <v-simple-checkbox
           v-model="item.is_allowed"
-          disabled
-        ></v-simple-checkbox>
+          @click="toggleInternet(item)"
+        />
       </template>
 
-      <template v-slot:item.actions="{ item }">
+      <template #item.actions="{ item }">
         <v-icon
           small
           class="mr-2"
@@ -158,6 +164,7 @@
 <script>
 export default {
   name: 'AdminUsers',
+  middleware: ['auth', 'admin'],
   data () {
     return {
       search: '',
@@ -168,7 +175,7 @@ export default {
           text: 'ID',
           align: 'start',
           sortable: false,
-          value: 'id',
+          value: 'id'
         },
         {
           text: 'Nom d\'utilisateur',
@@ -202,7 +209,7 @@ export default {
           text: 'Actions',
           value: 'actions',
           sortable: false
-        },
+        }
       ],
       users: [],
       editedIndex: -1,
@@ -213,7 +220,7 @@ export default {
         email: '',
         phone: '',
         role: '',
-        is_allowed: '',
+        is_allowed: ''
       },
       defaultItem: {
         username: '',
@@ -222,15 +229,15 @@ export default {
         email: '',
         phone: '',
         role: '',
-        is_allowed: '',
+        is_allowed: ''
       },
       rules: {
         required: value => !!value || 'Requis.',
-        email: value => {
+        email: (value) => {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
           return pattern.test(value) || 'Addresse email invalide.'
         },
-        phone: value => {
+        phone: (value) => {
           const pattern = /^\+33[67]\d{8}$|^0[67]\d{8}$/
           return pattern.test(value) || 'Numéro de téléphone invalide.'
         },
@@ -239,7 +246,19 @@ export default {
       loading: false
     }
   },
-  middleware: ['auth', 'admin'],
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'Nouveau utilisateur' : 'Editer l\'utilisateur'
+    }
+  },
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
+    }
+  },
   created () {
     this.$store.getters['api/users'].then(d => this.users = d)
   },
@@ -256,7 +275,17 @@ export default {
       this.dialogDelete = true
     },
 
+    toggleInternet (item) {
+      this.editedIndex = this.users.indexOf(item)
+      const n = {
+        id: this.users[this.editedIndex].id,
+        is_allowed: this.users[this.editedIndex].is_allowed
+      }
+      this.$store.getters['api/patch_user'](n)
+    },
+
     deleteItemConfirm () {
+      this.$store.getters['api/delete_user']({ id: this.users[this.editedIndex].id })
       this.users.splice(this.editedIndex, 1)
       this.closeDelete()
     },
@@ -279,31 +308,23 @@ export default {
 
     save () {
       if (this.editedIndex > -1) {
-        for (let k of Object.keys(this.editedItem)) {
+        const n = {
+          id: this.users[this.editedIndex].id
+        }
+        for (const k of Object.keys(this.editedItem)) {
           if (this.users[this.editedIndex][k] !== this.editedItem[k]) {
             console.log(`Value changed for key ${k}  ${this.users[this.editedIndex][k]} -> ${this.editedItem[k]}`)
+            n[k] = this.editedItem[k]
           }
         }
         Object.assign(this.users[this.editedIndex], this.editedItem)
+        this.$store.getters['api/patch_user'](n)
       } else {
         this.users.push(this.editedItem)
       }
       this.close()
-    },
-  },
-  computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'Nouveau utilisateur' : 'Editer l\'utilisateur'
-    },
-  },
-  watch: {
-    dialog (val) {
-      val || this.close()
-    },
-    dialogDelete (val) {
-      val || this.closeDelete()
-    },
-  },
+    }
+  }
 }
 </script>
 
